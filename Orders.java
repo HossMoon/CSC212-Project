f0aae16663bbc444bf1a8be50d45c3e32dcb0a4a
HossMoon <qihossam1@gmail.com>
2025-10-23 01:27:21 +0300

    Create Orders.java

---
+++ Orders.java
@@ -0,0 +1,12 @@
+public class Orders{
+  //orderId, customer reference, list of products, total price, order date,
+  //status (pending, shipped, delivered, canceled)
+
+
+  //Operations:
+  //Create/cancel order.
+
+  //Update order status.
+
+  //Search order by ID.
+}
