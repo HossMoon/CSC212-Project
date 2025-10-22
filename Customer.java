911ce0dc6dd1a35248097faa06a26da57f62fcbc
HossMoon <qihossam1@gmail.com>
2025-10-23 01:24:49 +0300

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,15 @@
+public class Customer{
+  int customerId:
+  String name;
+  String email;
+  // orders list
+
+  
+  //Operations:
+  //Register new customer
+
+  //Place a new order for a specific customer
+
+  //View order history
+  
+}
