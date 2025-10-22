2362e08986d6a1279fd2c8cea614e902c420396d
HossMoon <qihossam1@gmail.com>
2025-10-23 01:31:11 +0300

    Update and rename products.java to Products.java

---
+++ Products.java
@@ -0,0 +1,12 @@
+public class Products(){
+  //Attributes: productId, name, price, stock, list of reviews
+
+
+  //Operations:
+  //Add/remove/update products.
+
+  //Search by ID or name (linear).
+
+  //Track out-of-stock products.
+  
+}
