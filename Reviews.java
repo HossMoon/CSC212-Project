d13a275fe7cd41bd80ae65263b94a99ffa862da2
HossMoon <qihossam1@gmail.com>
2025-10-23 01:29:34 +0300

    Create Reviews.java

---
+++ Reviews.java
@@ -0,0 +1,11 @@
+public class Reviews{
+  //Attributes: rating score (1-5) and text comment.
+  //Linked to products.
+
+
+  //Operations:
+  //Add/edit review.
+
+  //Get an average rating for product.
+
+}
