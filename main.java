a3ca26999c97b306e4e7af42dec56281fa8e78c8
HossMoon <qihossam1@gmail.com>
2025-10-25 21:54:28 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+public static void main(String[] args){
+  //Requirements
+
+  
+  // • Read data from CSV file that contains products, customers, orders, and reviews.
+  
+  // • You can add a product, customer, and place an order.
+  
+  // • Customers can add reviews to products.
+  
+  // • Extract reviews from a specific customer for all products with the most efficient linear data structure possible.
+  
+  // • Suggest "top 3 products" by average rating.
+  
+  // • All Orders between two dates
+  
+  // • Given two customers IDs, show a list of common products that have been reviewed with an average rating of more than 4 out of 5.
+
+}
