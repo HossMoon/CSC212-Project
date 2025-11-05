23652234577ef5c73f221533f2eb85098bd5e47f
qiHos <qiHossam1@gmail.com>
2025-11-05 22:21:39 +0300

    Final rough draft for my portion.

--- Product.java
+++ Product.java
@@ -3,18 +3,22 @@ public class Product {
     String name;
     double price;
     int stock;
+    Reviews reviews;
     public Product(int productId, String name, double price, int stock) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.stock = stock;
+        this.reviews = new Reviews();
     }
     public int getProductId() { return productId; }
     public String getName() { return name; }
     public double getPrice() { return price; }
     public int getStock() { return stock; }
+    public Reviews getReviews() { return reviews; }
     public void setProductId(int productId) { this.productId = productId; }
     public void setName(String name) { this.name = name; }
     public void setPrice(double price) { this.price = price; }
     public void setStock(int stock) { this.stock = stock; }
+    public void setReviews(Reviews reviews) { this.reviews = reviews; }
 }
